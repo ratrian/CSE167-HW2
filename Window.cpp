89306bc3d6ffdee0a37fe0dc4fbaebd500d3c410
Ramin Atrian <ratrian@ucsd.edu>
2020-11-02 23:11:42 -0800

    setup completed for part#2

--- Window.cpp
+++ Window.cpp
@@ -204,7 +204,12 @@ void Window::keyCallback(GLFWwindow* window, int key, int scancode, int action, 
 
 void Window::mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
 {
-	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+	{
+		//
+	}
+	else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+	{
 		//
 	}
 }
