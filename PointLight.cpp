8442028bc9ff85269b4582d3d609dfa6a5ec6a96
Ramin Atrian <ratrian@ucsd.edu>
2020-12-01 23:18:15 -0800

    refactored

--- PointLight.cpp
+++ PointLight.cpp
@@ -22,7 +22,7 @@ void PointLight::orbit(glm::vec3 direction, float rotAngle, glm::vec3 rotAxis)
 {
 	glm::mat4 mT = glm::translate(glm::mat4(1.0), direction);
 	glm::mat4 mR = glm::rotate(glm::mat4(1.0), rotAngle, rotAxis);
-	pos = mT * glm::vec4(pos, 1.0) * mT;
+	pos = mT * glm::vec4(pos, 1.0);
 	pos = mR * glm::vec4(pos, 1.0);
 }
 
